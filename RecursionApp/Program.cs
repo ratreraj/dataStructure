c77016a346753b18d20c9a0faa7350f2c27eebc1
raj <ratre.raj@gmail.com>
2022-04-02 01:13:58 +0530

    first

---
+++ RecursionApp/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace RecursionApp
+{
+
+class Program 
+{   
+   public static void Fun(int n)
+    {
+        // while(n>0)
+        // {
+            if(n>0)
+            {
+            int result = n*n;
+            Console. WriteLine($"{result}");
+            //n = n-1;
+            Fun(n-1);    
+            }         
+        //}
+    }
+    static void Main()
+    {
+        Fun(10);
+     
+    }
+
+
+}
+
+}
